23efd5e74a3b247387722809351c3494bcda4c67
=Islam Hassan <islam.abuaish@gmail.com>
2023-07-17 20:03:14 +0300

    cahgne url

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // config.js
-const API_URL = "http://localhost:3000"; // Replace this with your API URL
+const API_URL = process.env.NEXT_PUBLIC_BASE_URL; // Replace this with your API URL
 
 export default {
     BaseUrl :API_URL
