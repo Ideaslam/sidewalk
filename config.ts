b3a81758b06d06bf2f902124dcd5577a3a22f2c3
=Islam Hassan <islam.abuaish@gmail.com>
2023-07-17 20:18:38 +0300

    changed the domain 2

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // config.js
-const API_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://sidewalk.apiserver.co"; // Replace this with your API URL
+const API_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://sidewalk.apiserver.co"; // Replace this with your API URL
 
 export default {
     BaseUrl :API_URL
