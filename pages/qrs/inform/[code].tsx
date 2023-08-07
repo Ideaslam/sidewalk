506e6012c2ed95edb29a487ed9e9aacf07f77864
Islam Hassan <islam.abuaish@gmail.com>
2023-08-08 02:15:07 +0300

    remove ErrorHandling

--- pages/qrs/inform/[code].tsx
+++ pages/qrs/inform/[code].tsx
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import ErrorBoundary from '../../../components/ErrorHandling';
+ 
 import type {
     GetStaticPropsContext,
     GetStaticPropsResult,
